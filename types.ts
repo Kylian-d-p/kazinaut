8757f9caa0ec9e2e145b32ab08c77649266f243e
Kylian Dupouy Peron <kdperon@yahoo.com>
2024-12-07 00:42:10 +0100

    Add session type to validation schemas

--- types.ts
+++ types.ts
@@ -3,6 +3,9 @@ import { z } from "zod";
 export const types = {
   email: z.string().email(),
   password: z.string().min(6, "Le mot de passe doit être d'au moins 3 caractères").max(25, "Le mot de passe doit être de 25 caractères maximum"),
+  session: z.object({
+    userId: z.string(),
+  })
 };
 
 export const schemas = {
