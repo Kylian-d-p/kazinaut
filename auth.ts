709d507933ecca698b38f815ec0eea32af305fe1
Kylian Dupouy Peron <kdperon@yahoo.com>
2024-12-07 01:24:59 +0100

    Integrate Google and Nodemailer providers with NextAuth and configure Prisma adapter

--- auth.ts
+++ auth.ts
@@ -1,5 +1,24 @@
-import NextAuth from "next-auth"
- 
+import { prisma } from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import Nodemailer from "next-auth/providers/nodemailer";
+import { env } from "./lib/env";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-})
+  providers: [Google, Nodemailer({
+    server: {
+      host: env.EMAIL_SERVER_HOST,
+      port: env.EMAIL_SERVER_PORT,
+      auth: {
+        user: env.EMAIL_SERVER_USER,
+        pass: env.EMAIL_SERVER_PASSWORD,
+      },
+    },
+    from: env.EMAIL_FROM,
+  })],
+  adapter: PrismaAdapter(prisma),
+  pages: {
+    signIn: "/auth/signin",
+  },
+});
