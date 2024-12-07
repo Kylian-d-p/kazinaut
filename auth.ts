ad989802c2008114b3277ff519c215607920cf61
Kylian Dupouy Peron <kdperon@yahoo.com>
2024-12-07 02:00:29 +0100

    Add verifyRequest page to NextAuth configuration

--- auth.ts
+++ auth.ts
@@ -6,19 +6,23 @@ import Nodemailer from "next-auth/providers/nodemailer";
 import { env } from "./lib/env";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google, Nodemailer({
-    server: {
-      host: env.EMAIL_SERVER_HOST,
-      port: env.EMAIL_SERVER_PORT,
-      auth: {
-        user: env.EMAIL_SERVER_USER,
-        pass: env.EMAIL_SERVER_PASSWORD,
+  providers: [
+    Google,
+    Nodemailer({
+      server: {
+        host: env.EMAIL_SERVER_HOST,
+        port: env.EMAIL_SERVER_PORT,
+        auth: {
+          user: env.EMAIL_SERVER_USER,
+          pass: env.EMAIL_SERVER_PASSWORD,
+        },
       },
-    },
-    from: env.EMAIL_FROM,
-  })],
+      from: env.EMAIL_FROM,
+    }),
+  ],
   adapter: PrismaAdapter(prisma),
   pages: {
     signIn: "/auth/signin",
+    verifyRequest: "/auth/verify-request",
   },
 });
