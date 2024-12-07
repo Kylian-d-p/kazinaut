e115b1dbfcbb02a57e7d45e8ca5fe05111cf920e
Kylian Dupouy Peron <kdperon@yahoo.com>
2024-12-07 02:11:43 +0100

    Enhance Header component with session-based user authentication and improved layout

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,15 +1,28 @@
+import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ThemeToggle } from "../theme/toggle";
 
-export default function Header() {
+export default async function Header() {
+  const session = await auth();
+
   return (
-    <header className="h-20">
-      <span>Kazinaut</span>
-      <Link href="/auth/signin">
-        <Button>Se connecter</Button>
+    <header className="h-20 p-5 flex items-center justify-between border-b border-b-accent-foreground">
+      <Link href="/">
+        <span className="text-xl font-bold">Kazinaut</span>
       </Link>
-      <ThemeToggle/>
+      <div className="flex items-center gap-2">
+        {session && session.user ? (
+          <Link href="/account">
+            <Button>{session.user.name || "Mon Compte"}</Button>
+          </Link>
+        ) : (
+          <Link href="/auth/signin">
+            <Button>Se connecter</Button>
+          </Link>
+        )}
+        <ThemeToggle />
+      </div>
     </header>
   );
 }
