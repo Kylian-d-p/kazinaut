84e215ba2564d054d10baa5c8f7005690ad225d1
Kylian Dupouy Peron <kdperon@yahoo.com>
2024-12-07 10:56:14 +0100

    Replace static message with a button linking to games page for improved navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <div className="flex flex-col gap-10 items-center justify-center h-[calc(100vh-5rem)]">
         <h1 className="text-6xl">Kazinaut</h1>
-        <p className="bg-accent text-accent-foreground px-3 py-2 rounded-full cursor-default hover:brightness-110 transition-[filter]">Rien ne va plus !</p>
+        <Link href="/games">
+          <Button variant={"accent"} className="rounded-full">
+            Rien ne va plus !
+          </Button>
+        </Link>
       </div>
     </main>
   );
