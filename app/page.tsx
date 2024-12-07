43427437499035ed84c99dfe2f5f7c1513b2fc05
Kylian Dupouy Peron <kdperon@yahoo.com>
2024-12-07 02:18:06 +0100

    restyle - WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <main>
-      <h1>Kazinaut</h1>
+      <div className="flex flex-col gap-10 items-center justify-center h-[calc(100vh-5rem)]">
+        <h1 className="text-6xl">Kazinaut</h1>
+        <p className="bg-accent text-accent-foreground px-3 py-2 rounded-full cursor-default hover:brightness-110 transition-[filter]">Rien ne va plus !</p>
+      </div>
     </main>
   );
 }
