34b294fd33a92a6329251ea71b2cfe31df23817b
Kylian Dupouy Peron <kdperon@yahoo.com>
2024-12-07 02:02:33 +0100

    Refactor VerifyRequest component to use Card layout for improved UI structure

--- app/auth/verify-request/page.tsx
+++ app/auth/verify-request/page.tsx
@@ -1,10 +1,18 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function VerifyRequest() {
   return (
     <main className="flex justify-center">
-      <div className="flex flex-col items-center gap-5 rounded-lg bg-accent p-5">
-        <h1 className="text-3xl">Vérifiez votre adresse mail</h1>
-        <p className="text-muted-foreground">Un lien vous a été envoyé.</p>
-      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            <h1 className="text-3xl">Vérifiez votre adresse mail</h1>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-muted-foreground">Un lien vous a été envoyé.</p>
+        </CardContent>
+      </Card>
     </main>
   );
 }
