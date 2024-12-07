6fd5932af8e18e59730424af3f7de9b9bf20c174
Kylian Dupouy Peron <kdperon@yahoo.com>
2024-12-07 01:52:17 +0100

    Add VerifyRequest component for email verification page

---
+++ app/auth/verify-request/page.tsx
@@ -0,0 +1,10 @@
+export default function VerifyRequest() {
+  return (
+    <main className="flex justify-center">
+      <div className="flex flex-col items-center gap-5 rounded-lg bg-accent p-5">
+        <h1 className="text-3xl">Vérifiez votre adresse mail</h1>
+        <p className="text-muted-foreground">Un lien vous a été envoyé.</p>
+      </div>
+    </main>
+  );
+}
