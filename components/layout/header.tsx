edf6ec4f7e5ab4df0565b7ac3ba643bba228446c
Kylian Dupouy Peron <kdperon@yahoo.com>
2024-12-07 15:44:41 +0100

    Make header component fixed and add spacing for content below

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -7,22 +7,25 @@ export default async function Header() {
   const session = await auth();
 
   return (
-    <header className="h-20 p-5 flex items-center justify-between border-b border-b-accent-foreground">
-      <Link href="/">
-        <span className="text-xl font-bold">Kazinaut</span>
-      </Link>
-      <div className="flex items-center gap-2">
-        {session && session.user ? (
-          <Link href="/account">
-            <Button>{session.user.name || "Mon Compte"}</Button>
-          </Link>
-        ) : (
-          <Link href="/auth/signin">
-            <Button>Se connecter</Button>
-          </Link>
-        )}
-        <ThemeToggle />
-      </div>
-    </header>
+    <>
+      <header className="h-20 p-5 flex items-center justify-between border-b border-b-accent-foreground fixed top-0 left-0 right-0 bg-background z-50">
+        <Link href="/">
+          <span className="text-xl font-bold">Kazinaut</span>
+        </Link>
+        <div className="flex items-center gap-2">
+          {session && session.user ? (
+            <Link href="/account">
+              <Button>{session.user.name || "Mon Compte"}</Button>
+            </Link>
+          ) : (
+            <Link href="/auth/signin">
+              <Button>Se connecter</Button>
+            </Link>
+          )}
+          <ThemeToggle />
+        </div>
+      </header>
+      <div className="h-20" />
+    </>
   );
 }
